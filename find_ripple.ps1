97e4b709dc5128b42a54fe4c0e0e5d0bdb18a6e4
tubohu <dengyangfan1203@outlook.com>
2026-06-20 02:41:56 +0800

    Delete find_ripple.ps1

--- find_ripple.ps1
+++
@@ -1,6 +0,0 @@
-Get-ChildItem 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall' | ForEach-Object {
-    $props = Get-ItemProperty $_.PSPath -ErrorAction SilentlyContinue
-    if ($props.Publisher -eq 'Ripple') {
-        Write-Output "$($_.PSChildName)  $($props.DisplayName)  $($props.DisplayVersion)"
-    }
-}
